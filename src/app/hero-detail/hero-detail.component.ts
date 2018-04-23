be8d510261da618d2c9d3792be399e0d444187c1
jessepackwood <packwoodjesse@gmail.com>
2018-04-23 08:14:04 -0600

    create hero-detail component

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
@@ -6,8 +6,9 @@ import { Hero } from '../hero';
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
 
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
   constructor() { }
 
   ngOnInit() {
